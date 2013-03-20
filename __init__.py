5725f929fef1db5a1e20640f420c87ac893bd8c3
resteve <resteve@zikzakmedia.com>
2013-03-20 14:05:40 +0100

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#This file is part sale_asterisk module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
+
+from trytond.pool import Pool
+
+
+def register():
+    pass
+
---
+++ doc/index.rst
@@ -0,0 +1,4 @@
+Sale Asterisk Module
+#####################
+
+The sale_asterisk module is example tryton module.
