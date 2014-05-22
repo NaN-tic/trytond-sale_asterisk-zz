53080329aaf00dc408dc5568b7ca1b9d57914608
NaN?tic <suport@nan-tic.com>
2014-05-22 15:41:25 +0200

    Resolve some bugs produceds in the merge

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import Pool
-from .opportunity import *
+from .sale import *
 
 def register():
     Pool.register(
