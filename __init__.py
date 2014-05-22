d99be4c03e2477dd5b53176efa74340e9e6ee182
NaN?tic <suport@nan-tic.com>
2014-05-22 15:44:05 +0200

    Resolve some bugs produceds in the merge

--- __init__.py
+++ __init__.py
@@ -3,10 +3,12 @@
 #the full copyright notices and license terms.
 from trytond.pool import Pool
 from .sale import *
+from .contact_mechanism import *
 
 def register():
     Pool.register(
         SaleAsteriskResult,
+        ContactMechanism,
         module='sale_asterisk', type_='model')
     Pool.register(
         SaleAsterisk,
